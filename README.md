ae232070bae8bf4dfa8d7ecbc2ac548f102884e5
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-02-17 15:01:11 +1100

    - Fully working program. - No errors found.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# tic-tac-toe
+# Ruby: tic-tac-toe
+
+## 1. Description 
+Game of tic-tac-toe where two players plays against each other. This is a simple command prompt game with use of class and inheritance in Obejct Oriented Programming. 
+
+## 1. Usage
+1. Run the program in command prompt with follow prompt:
+``` ruby tic_tac_toe.rb ```
+2. 3x3 board will be displayed each with corresponding numbers. 
+``` 
+-------
+1  2  3   
+4  5  6   
+7  8  9  
+-------
+```
+3. Player 1 (X) mark position on the board by typing in matching number. 
+``` Player X mark position: ```
+4. Board will be displayed again showing Players mark.
+5. Player 2 (O) mark position on the board by typing in matching number. 
+    - If player input does not match numbers on the board program will re-prompt for user input. 
+    - ``` Invalid number/Position taken: ```
+6. Taking turn this process will be repeated until horizontal, vertical or cross line is formed of same mark(X or O)
+7. If winning line is found program will declare player as winner and end. 
+```
+-------
+O  X  3   
+O  X  6   
+7  X  9  
+-------
+Player X WINS!
+```
+8. If no winner is found after 9 turns program will declare as draw and end. 
+
+## Future Update/Add-ons
+1. Implement class #Player and ask for user name and create new instanc of player. 
+
+## Thoughts on the project
+- I've created parent class `Board` with child class of `Game`. The game is started by instance of child class `Game`. Looking back at my solution I feel `Game` and `Board` should be separate with no dependency. 
+
+- After looking at different solutions I found maybe `board` hash is not a good solution. Maybe just simple 2D array? 
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,25 +1,32 @@
 # 15/02/2024 Tic-Tac-Toe
 
-class Game
+class Board
+  # Players signs & Rounds
   PLAYER_1 = "X"
   PLAYER_2 = "O"
+  ROUNDS = 10
+
+  # Board in hash
+  @@board = { 1 => nil, 2 => nil, 3 => nil,
+              4 => nil, 5 => nil, 6 => nil,
+              7 => nil, 8 => nil, 9 => nil }
+  # Winning lines
+  @@line = [ [1, 2, 3], [4, 5, 6], [7, 8, 9],
+             [1, 4, 7], [2, 5, 8], [3, 6, 9],
+             [1, 5, 9], [3, 5, 7] ]
+
 
   def initialize
-    @board = { 1 => nil, 2 => nil, 3 => nil,
-    4 => nil, 5 => nil, 6 => nil,
-    7 => nil, 8 => nil, 9 => nil }
-
-    @winning = [ [1, 2, 3], [4, 5, 6], [7, 8, 9],
-                 [1, 4, 7], [2, 5, 8], [3, 6, 9],
-                 [1, 5, 9], [3, 5, 9] ]
-
-    start
+    puts "-------"
+    puts "Player 1 is (X) and Player 2 is (O)"
+    display_board
   end
 
+  # Display board and update
   def display_board
     puts "-------"
     count = 0
-    @board.select do |key, value|
+    @@board.select do |key, value|
       if value == nil
         print "#{key}  "
       else
@@ -33,73 +40,80 @@ class Game
     puts ""
     puts "-------"
   end
-
-  def start
-    puts "-------"
-    puts "Player 1 is (X) and Player 2 is (O)"
-    display_board
-  end
 end
 
-class PlayGame
-  attr_accessor :player_choice, :switch_turns
+class Game < Board
+  attr_accessor :switch_turns
 
   def initialize
-    @new_game = Game.new
-    @player_choice = nil
+    @new_board = Board.new
     @switch_turns = false
-    start_round
+    play_game
   end
-
-  def start_round
-    i = 0
-    loop do
+  private
+  def play_game
+    ROUNDS.times do |i|
       if i == 9
-        break
+        puts "It's a Draw!"
+        game_end
       end
+      # Input from player or switched player
       if switch_turns == false
-        puts "Player 1 pick a number: "
-        self.player_choice = check_choice(gets.chomp)
-        update_round(PLAYER_1, player_choice)
-        check_winner(PLAYER_1)
+        print "Player X mark position: "
+        update_game(PLAYER_1, check_choice(gets.chomp))
       else
-        puts "Player 2 pick a number: "
-        self.player_choice = check_choice(gets.chomp)
-        update_round(PLAYER_2, player_choice)
-        check_winner(PLAYER_2)
+        print "Player O mark position: "
+        update_game(PLAYER_2, check_choice(gets.chomp))
       end
-      i += 1
     end
   end
 
-  def update_round(player, player_choice)
+  # Update the game and check for winner.
+  def update_game(player, choice)
     @@board.select do |key, value|
-      if key == player_choice.to_i && !value
+      if key == choice.to_i
         @@board[key] = player
       end
     end
-    self.switch_turns = !switch_turns
-    display_board
+    self.switch_turns = !switch_turns   # Switch player.
+    display_board # Display current board
+
+    # Check for winner.
+    if is_winner(player)
+      puts "Player #{player} WINS!"
+      game_end
+    end
   end
 
+  # Checks if player input is valid and reprompt if invalid
   def check_choice(choice)
     if @@board.has_key?(choice.to_i) && @@board.values_at(choice.to_i) == [nil]
       return choice
     else
-      puts "Invalid number or Position taken:  "
+      print "Invalid number/Position taken:  "
       check_choice(gets.chomp)
     end
   end
 
-  def check_winner(player)
-    player_combo = @@board.select {|key, value| value == player}.keys
+  # Checks if there is winner if found exit
+  def is_winner(player)
+    # Collect current player positions
+    player_line = @@board.select {|key, value| value == player}.keys
 
-    @@winning.select do |win_combo|
-      return player_combo.sort.join("").include?(win_combo.join(""))
+    @@line.collect do |winning_line|
+      if (winning_line - player_line).empty? # If empty then is winner.
+        return true
+      end
     end
+
+    return false
+  end
+
+  # End game
+  def game_end
+    exit
   end
 
 end
 
-new_game = PlayGame.new()
-new_game
+new_game = Game.new()
